d33225bbdde04d75a993b69c6b46b09112b4e11c
kevinct1 <kevincastillot@gmail.com>
2026-05-15 22:00:40 -0400

    SW v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='altura-cafe-v11';
+const CACHE='altura-cafe-v12';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
